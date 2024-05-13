cfb99745c98880410ac2fbdf7f829dc035bb95df
Ismael S. Kafeltz <shalanga@gmail.com>
2024-05-13 15:58:45 -0300

    added first reverse challenge

---
+++ First_Reverse.js
@@ -0,0 +1,11 @@
+// https://coderbyte.com/results/ismaelkafeltz:First%20Reverse:JavaScript
+
+function FirstReverse(str) { 
+
+  // code goes here  
+  return str.split('').reverse().join(''); 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
